f318866f74984eec7688fcd25f8de81096d8f554
Domon <viplizhao@gmail.com>
2013-12-31 20:50:30 +0800

    update
    20131231

--- gen/com/example/sokoban/R.java
+++ gen/com/example/sokoban/R.java
@@ -21,11 +21,23 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int login_background=0x7f020001;
+        public static final int btn_exit=0x7f020000;
+        public static final int btn_help=0x7f020001;
+        public static final int btn_option=0x7f020002;
+        public static final int btn_start=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int login_background=0x7f020005;
+        public static final int main_bg=0x7f020006;
+        public static final int sokoban=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int Btn_Exit=0x7f080007;
+        public static final int Btn_Help=0x7f080006;
+        public static final int Btn_Option=0x7f080005;
+        public static final int Btn_Start=0x7f080004;
+        public static final int Btns_Layout=0x7f080003;
+        public static final int ImageView=0x7f080002;
+        public static final int action_settings=0x7f080008;
         public static final int pBar=0x7f080000;
         public static final int textview=0x7f080001;
     }
@@ -38,6 +50,10 @@ public final class R {
         public static final int main=0x7f070001;
     }
     public static final class string {
+        public static final int Btn_Exit=0x7f050008;
+        public static final int Btn_Help=0x7f050007;
+        public static final int Btn_Option=0x7f050006;
+        public static final int Btn_Start=0x7f050005;
         public static final int Login_Text=0x7f050004;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
--- src/com/example/sokoban/MainActivity.java
+++ src/com/example/sokoban/MainActivity.java
@@ -1,15 +1,70 @@
 package com.example.sokoban;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	// 主界面的button
+	private Button startButton;
+	private Button optionButton;
+	private Button helpButton;
+	private Button exitButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		// 设置全屏
+		Window window = MainActivity.this.getWindow();
+		requestWindowFeature(window.FEATURE_NO_TITLE);
+		window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
+
+		startButton = (Button) findViewById(R.id.Btn_Start);
+		optionButton = (Button) findViewById(R.id.Btn_Option);
+		helpButton = (Button) findViewById(R.id.Btn_Help);
+		exitButton = (Button) findViewById(R.id.Btn_Exit);
+
+		// 退出按钮点击事件
+		exitButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// 新建一个对话框提示用户
+				new AlertDialog.Builder(MainActivity.this)
+						.setTitle("提示")
+						.setMessage("确认退出吗?")
+						// 点击确认关闭对话框
+						.setPositiveButton("确认",
+								new DialogInterface.OnClickListener() {
+									@Override
+									public void onClick(DialogInterface dialog,
+											int which) {
+										// TODO Auto-generated method stub
+										dialog.dismiss();
+										MainActivity.this.finish();
+									}
+								})
+						// 点击取消返回
+						.setNegativeButton("取消",
+								new DialogInterface.OnClickListener() {
+									@Override
+									public void onClick(DialogInterface dialog,
+											int which) {
+										// TODO Auto-generated method stub
+										dialog.dismiss();
+									}
+								}).create().show();
+			}
+		});
 	}
 
 	@Override
